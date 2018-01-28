da14dee470d6fa8fcba30e64c70709faa3de5cf3
afaulkner420 <afaulkner420@gmail.com>
2018-01-28 00:08:00 +0000

    Initial Commit

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES_64 := lib64/libion_mtk.so
+LOCAL_SRC_FILES_32 := lib/libion_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
---
+++ power_3-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/ulefone/power_3/power_3-vendor-blobs.mk)
