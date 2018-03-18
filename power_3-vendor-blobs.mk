1f9a2b5e00f0d4e4b57cc0a2b4500a568e597316
afaulkner420 <afaulkner420@gmail.com>
2018-03-18 22:01:01 +0000

    move sw_config to device tree

--- power_3-vendor-blobs.mk
+++ power_3-vendor-blobs.mk
@@ -1,7 +1,5 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/power_3/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/power_3/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/power_3/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/power_3/vendor,system/vendor)
